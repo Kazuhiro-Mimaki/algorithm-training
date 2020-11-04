69caf8aa7c971e4036685e1f811c8d397dfcc8ba
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2020-11-04 21:28:50 +0900

    精選4

---
+++ AtCoder/10_problems_from_Qiita/4.py
@@ -0,0 +1,24 @@
+A = input()
+B = input()
+C = input()
+X = input()
+A = int(A)
+B = int(B)
+C = int(C)
+X = int(X)
+
+sum = 0
+i = 0
+j = 0
+for i in range(A+1):
+  for j in range(B+1):
+    for k in range(C+1):
+      if i * 500 + j * 100 + k * 50 == X:
+        sum += 1
+print(sum)
+
+# result = a * b % 2
+# if result == 0:
+#   print('Even')
+# else:
+#   print('Odd')
