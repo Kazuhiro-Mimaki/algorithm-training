8f332b04b0abe57fbffe2899eb6d6a173b1bf414
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2020-11-04 21:34:23 +0900

    計算のまとまりをわかりやすく

--- AtCoder/10_problems_from_Qiita/4.py
+++ AtCoder/10_problems_from_Qiita/4.py
@@ -13,7 +13,7 @@ j = 0
 for i in range(A+1):
   for j in range(B+1):
     for k in range(C+1):
-      if i * 500 + j * 100 + k * 50 == X:
+      if i*500 + j*100 + k*50 == X:
         sum += 1
 print(sum)
 
